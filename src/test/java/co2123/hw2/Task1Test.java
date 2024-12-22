6aed384373657a5a114ebd6060a2ba4e3845145a
raau1 <raulblancovazquezcontact@gmail.com>
2024-12-22 18:44:51 +0000

    Add repo and classes

--- src/test/java/co2123/hw2/Task1Test.java
+++ src/test/java/co2123/hw2/Task1Test.java
@@ -20,13 +20,23 @@ public class Task1Test {
         assertTrue("URl for the database not defined", p.getProperty("spring.datasource.url") != null);
         assertTrue("Username for the database not defined", p.getProperty("spring.datasource.username") != null);
         assertTrue("Password for the database not defined", p.getProperty("spring.datasource.password") != null);
+        assertTrue("Missing/Incomplete configuration for printing out SQL to the command line with spring.jpa.show-sql.",p.getProperty("spring.jpa.show-sql").equals("true"));
+
+        String s = p.getProperty("logging.level.org.hibernate.SQL");
+        if(s != null){
+            s = s.toLowerCase();
+            assertTrue("Missing/Incomplete configuration for printing out SQL to the command line with logging.level.org.hibernate.SQL.",s.contains("debug"));
+        } else {
+            assertTrue("Missing/Incomplete configuration for printing out SQL to the command line with logging.level.org.hibernate.SQL.",p.getProperty("logging.level.org.hibernate.SQL").contains("debug"));
+        }
     }
 
     @Test
     public void dependencies() throws Exception {
         String content = Files.readString(Paths.get("build.gradle"));
         assertTrue("Jasper libraries not loaded for JSPs.", content.contains("tomcat-embed-jasper"));
-        assertTrue("Missing a dependency for connecting to the database.", content.contains("mysql-connector-java"));
+        assertTrue("JSTL library not included in build.gradle.", content.contains("jstl"));
+        assertTrue("Missing a dependency for connecting to the database.", content.contains("mysql-connector"));
         assertTrue("Missing a dependency for connecting to the database.", content.contains("spring-boot-starter-data-jpa"));
     }
 
